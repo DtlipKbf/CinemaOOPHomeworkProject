a4c2e052fdf07dc20bedf7faad2bc458d56867af
DtlipKbf <126586620+DtlipKbf@users.noreply.github.com>
2023-04-04 18:41:43 +0300

    Update main.cpp

--- CinemaOOPHomeworkProject/main.cpp
+++ CinemaOOPHomeworkProject/main.cpp
@@ -1,7 +1,29 @@
-#include "main.h">
+#include "main.h"
+
+class Cinema {
+public:
+	string name;
+	int time;
+	float ticket_price;	
+	int row;
+	int place;
+};
 
 int main() {
+	Cinema cn1;
+	cn1.name = "Cat in boots 2";
+	cn1.time = 100;
+	cn1.ticket_price = 9.90;
+	cn1.row = 7;
+	cn1.place = 12;
 
+	Cinema cn2;
+	cn2.name = "Avatar: Path of Water";
+	cn2.time = 192;
+	cn2.ticket_price = 10.20;
+	cn2.row = 8;
+	cn2.place = 11;
+	
 	system("pause");
 	return 0;
 }
